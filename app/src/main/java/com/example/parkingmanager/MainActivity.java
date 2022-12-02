3e3b46eeebdf00fb235221f09d105cdbc7c3afc8
daniel <danthienphuc@gmail.com>
2022-12-03 03:49:42 +0700

    update splash_screen_sound

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.parkingmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.whoosh);
+        mp.setVolume(0.2f, 0.2f);
+        mp.start();
 
         ImageView img = (ImageView)findViewById(R.id.img_logo_splash);
         Animation aniImage = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_screen_ani);
