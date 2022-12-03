42225dc7ad455b0d3d6d70625a1057138c69db30
hao759 <qhao74154@gmail.com>
2022-12-03 13:58:00 +0700

    test

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -42,6 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2499);
+        }, 2500);
     }
 }
