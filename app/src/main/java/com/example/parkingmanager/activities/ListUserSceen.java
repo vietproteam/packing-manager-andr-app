41aa400fe98738e80fb704a835b5cf29460e517a
hao759 <qhao74154@gmail.com>
2022-12-06 20:37:56 +0700

    ma user screen

---
+++ app/src/main/java/com/example/parkingmanager/activities/ListUserSceen.java
@@ -0,0 +1,112 @@
+//package com.example.parkingmanager.activities;
+//
+//import java.util.ArrayList;
+//
+//import android.annotation.SuppressLint;
+//import android.os.Bundle;
+//import android.app.Activity;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+//import android.widget.CheckBox;
+//import android.widget.EditText;
+//import android.widget.ImageButton;
+//import android.widget.ListView;
+//import android.widget.RadioGroup;
+//
+//import com.example.parkingmanager.R;
+//
+//public class ListUserScreen extends Activity {
+//
+//    ArrayList<User> arrEmployee=new ArrayList<User>();
+//    //Sử dụng MyArrayAdapter thay thì ArrayAdapter
+//    myadappter adapter=null;
+//    ListView lvNhanvien=null;
+//
+//    Button btnNhap;
+//    ImageButton btnRemoveAll;
+//    EditText editMa,editTen;
+//    RadioGroup genderGroup;
+//
+//    @SuppressLint("MissingInflatedId")
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list_user_sceen);
+//
+//        btnNhap=(Button) findViewById(R.id.btnEdit);
+//        btnRemoveAll=(ImageButton) findViewById(R.id.btndelete);
+//        editMa=(EditText) findViewById(R.id.txtMaNV);
+//        editTen=(EditText) findViewById(R.id.txtTenNV);
+//        genderGroup=(RadioGroup) findViewById(R.id.radioGroup1);
+//
+//        lvNhanvien=(ListView) findViewById(R.id.imgitem);
+//        arrEmployee=new ArrayList<User>();
+//        //Khởi tạo đối tượng adapter và gán Data source
+//        adapter=new myadappter(
+//                this,
+//                R.layout.mylistview,// lấy custom layout
+//                arrEmployee/*thiết lập data source*/);
+//        lvNhanvien.setAdapter(adapter);//gán Adapter vào Lisview
+//
+//        btnNhap.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View arg0) {
+//                // TODO Auto-generated method stub
+//                xulyNhap();
+//            }
+//        });
+//        btnRemoveAll.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View arg0) {
+//                xulyXoa();
+//            }
+//        });
+//    }
+//    //gọi hàm xử lý nhập thông tin nhân viên
+//    public void xulyNhap()
+//    {
+//        String ma=editMa.getText()+"";
+//        String ten=editTen.getText()+"";
+//        boolean gioitinh=false;//Nam =false
+//        if(genderGroup.getCheckedRadioButtonId()==R.id.rdoNu)
+//            gioitinh=true;
+//        //Tạo một employee
+//        User emp=new User();
+//        emp.setId(ma);
+//        emp.setName(ten);
+//        emp.setGender(gioitinh);
+//        //Đưa vào danh sách
+//        arrEmployee.add(emp);
+//        //gọi hàm cập nhật giao diện
+//        adapter.notifyDataSetChanged();
+//        //Sau khi update thì xóa trắng dữ liệu và cho editma focus
+//        editMa.setText("");
+//        editTen.setText("");
+//        editMa.requestFocus();
+//    }
+//    //hàm xử lý xóa
+//    public void xulyXoa()
+//    {
+//        //ta nên đi ngược danh sách, kiểm tra phần tử nào checked
+//        //thì xóa đúng vị trí đó ra khỏi arrEmployee
+//        for(int i=lvNhanvien.getChildCount()-1;i>=0;i--)
+//        {
+//            //lấy ra dòng thứ i trong ListView
+//            //Dòng thứ i sẽ có 3 phần tử: ImageView, TextView, Checkbox
+//            View v=lvNhanvien.getChildAt(i);
+//            //Ta chỉ lấy CheckBox ra kiểm tra
+////            CheckBox chk=(CheckBox) v.findViewById(R.id.chkitem);
+//            //Nếu nó Checked thì xóa ra khỏi arrEmployee
+////            if(chk.isChecked())
+////            {
+////                //xóa phần tử thứ i ra khỏi danh sách
+////                arrEmployee.remove(i);
+////            }
+//        }
+//        //Sau khi xóa xong thì gọi update giao diện
+//        adapter.notifyDataSetChanged();
+//    }
+//}
