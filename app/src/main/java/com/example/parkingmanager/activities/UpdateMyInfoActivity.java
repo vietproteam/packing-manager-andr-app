e2563603fcac3e0a671971ac4e075e512e4d86b6
qhung <qhung08080@gmail.com>
2022-12-11 15:08:08 +0700

    Add MyInfo

--- app/src/main/java/com/example/parkingmanager/activities/UpdateMyInfoActivity.java
+++ app/src/main/java/com/example/parkingmanager/activities/UpdateMyInfoActivity.java
@@ -2,15 +2,90 @@ package com.example.parkingmanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.parkingmanager.R;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.io.IOException;
+import java.security.Permission;
+import java.util.List;
+
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import gun0912.tedbottompicker.TedBottomPicker;
+
 
 public class UpdateMyInfoActivity extends AppCompatActivity {
+private ImageButton imageButton;
+private CircleImageView circleImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_my_info);
+
+        imageButton =(ImageButton) findViewById(R.id.btnCamera);
+        circleImageView =(CircleImageView) findViewById(R.id.profile_image);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestPermissions();
+            }
+        });
+
+    }
+    private void requestPermissions(){
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+
+                openImagePicker();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(UpdateMyInfoActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+
+    }
+
+    public void openImagePicker(){
+        TedBottomPicker.OnImageSelectedListener listener = new TedBottomPicker.OnImageSelectedListener() {
+            @Override
+            public void onImageSelected(Uri uri) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    circleImageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+//        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(UpdateMyInfoActivity.this)
+//                .setOnImageSelectedListener(listener)
+//                .create();
+//        tedBottomPicker.show(getSupportFragmentManager());
+
+
     }
 }
