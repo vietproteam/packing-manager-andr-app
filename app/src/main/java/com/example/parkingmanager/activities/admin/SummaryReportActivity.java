a655a36946a27ed5e3a7c191771ad38e606b2602
qhung <qhung08080@gmail.com>
2022-12-25 16:33:23 +0700

    Report

--- app/src/main/java/com/example/parkingmanager/activities/admin/SummaryReportActivity.java
+++ app/src/main/java/com/example/parkingmanager/activities/admin/SummaryReportActivity.java
@@ -5,19 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.parkingmanager.R;
 import com.example.parkingmanager.activities.fragments.MenuAdminFragment;
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class SummaryReportActivity extends AppCompatActivity {
-
+public class SummaryReportActivity extends AppCompatActivity implements OnChartValueSelectedListener {
+    private PieChart mChart;
+    private CombinedChart cbtChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,34 +55,163 @@ public class SummaryReportActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, menuAdminFragment).commit();
 
+        MChart();
+        CombinedChart();
+        mChart.setOnChartValueSelectedListener(this);
+        cbtChart.setOnChartValueSelectedListener(this);
+        YAxis rightAxis = cbtChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setAxisMinimum(0f);
+
+        YAxis leftAxis = cbtChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinimum(0f);
+
+        final List<String> xLabel = new ArrayList<>();
+        xLabel.add("Jan");
+        xLabel.add("Feb");
+        xLabel.add("Mar");
+        xLabel.add("Apr");
+        xLabel.add("May");
+        xLabel.add("Jun");
+        xLabel.add("Jul");
+        xLabel.add("Aug");
+        xLabel.add("Sep");
+        xLabel.add("Oct");
+        xLabel.add("Nov");
+        xLabel.add("Dec");
+
+        XAxis xAxis = cbtChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return xLabel.get((int) value % xLabel.size());
+            }
+        });
+
+        CombinedData data = new CombinedData();
+        LineData lineDatas = new LineData();
+        lineDatas.addDataSet((ILineDataSet) dataChart());
+
+        data.setData(lineDatas);
+
+        xAxis.setAxisMaximum(data.getXMax() + 0.25f);
+
+        cbtChart.setData(data);
+        cbtChart.invalidate();
+    }
 
 
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Toast.makeText(this, "Value: "
+                + e.getY()
+                + ", index: "
+                + h.getX()
+                + ", DataSet index: "
+                + h.getDataSetIndex(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu( Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_admin, menu);
-        return true;
-    }
-    @Override
+    public void onNothingSelected() {
 
-    public boolean onOptionsItemSelected( MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_user_management:
-                return true;
-            case R.id.action_report:
-                return true;
-            case R.id.action_settings:
-                return true;
-            case R.id.action_logout:
-                Intent intent = new Intent( this, SuperLoginActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    }
+
+    private static void addDataSet(PieChart pieChart) {
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+        float[] yData = {25, 40, 70};
+        String[] xData = {"January", "February", "January"};
+
+        for (int i = 0; i < yData.length; i++) {
+            yEntrys.add(new PieEntry(yData[i], i));
         }
+        for (int i = 0; i < xData.length; i++) {
+            xEntrys.add(xData[i]);
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Employee Sales");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+
+        pieDataSet.setColors(colors);
+
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
+    public void MChart() {
+        mChart = (PieChart) findViewById(R.id.piechart);
+        mChart.setRotationEnabled(true);
+        mChart.setDescription(new Description());
+        mChart.setHoleRadius(35f);
+        mChart.setTransparentCircleAlpha(0);
+        mChart.setCenterText("PieChart");
+        mChart.setCenterTextSize(10);
+        mChart.setDrawEntryLabels(true);
+
+        addDataSet(mChart);
+
+        mChart.setOnChartValueSelectedListener(this);
+    }
+    public void CombinedChart() {
+        cbtChart = (CombinedChart) findViewById(R.id.combinedchart);
+        cbtChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE
+        });
+        cbtChart.getDescription().setEnabled(false);
+        cbtChart.setDrawGridBackground(false);
+        cbtChart.setDrawBarShadow(false);
+        cbtChart.setHighlightFullBarEnabled(false);
+        cbtChart.animateY(2500);
+        cbtChart.getLegend().setEnabled(false);
+        cbtChart.setOnChartValueSelectedListener(this);
+
+    }
+
+
+
+    private static DataSet dataChart() {
+
+        LineData d = new LineData();
+        int[] data = new int[] { 1, 2, 2, 1, 1, 1, 2, 1, 1, 2, 1, 9 };
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        for (int index = 0; index < 12; index++) {
+            entries.add(new Entry(index, data[index]));
+        }
+
+        LineDataSet set = new LineDataSet(entries, "Request Ots approved");
+        set.setColor(Color.GREEN);
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.GREEN);
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.GREEN);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.GREEN);
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        d.addDataSet(set);
+
+        return set;
+    }
+
 
 
 
