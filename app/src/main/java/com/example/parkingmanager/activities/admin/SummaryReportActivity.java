73b2d2d1ab24caea373b509fb009775fd94fe42c
qhung <qhung08080@gmail.com>
2022-12-25 14:08:03 +0700

    menu_admin

--- app/src/main/java/com/example/parkingmanager/activities/admin/SummaryReportActivity.java
+++ app/src/main/java/com/example/parkingmanager/activities/admin/SummaryReportActivity.java
@@ -1,33 +1,48 @@
 package com.example.parkingmanager.activities.admin;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.parkingmanager.R;
+import com.example.parkingmanager.activities.fragments.MenuAdminFragment;
+
 
 public class SummaryReportActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_summary_report);
+
+        MenuAdminFragment menuAdminFragment = new MenuAdminFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, menuAdminFragment).commit();
+
+
+
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
+    public boolean onCreateOptionsMenu( Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_admin, menu);
+        return true;
     }
-
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
+
+    public boolean onOptionsItemSelected( MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_user_management:
                 return true;
@@ -36,12 +51,14 @@ public class SummaryReportActivity extends AppCompatActivity {
             case R.id.action_settings:
                 return true;
             case R.id.action_logout:
-                Intent intent = new Intent(this, SuperLoginActivity.class);
+                Intent intent = new Intent( this, SuperLoginActivity.class);
                 startActivity(intent);
-                this.finish();
                 return true;
             default:
-                return super.onContextItemSelected(item);
+                return super.onOptionsItemSelected(item);
         }
     }
+
+
+
 }
